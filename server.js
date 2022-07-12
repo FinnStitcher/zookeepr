99666a05d099f2a29bb65424f2d0fbd224f38950
Finn Stitcher <foundfootage@protonmail.com>
2022-07-11 17:00:50 -0700

    server set-up

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const { animals } = require("./data/animals");
+
+const app = express();
+
+function filterByQuery(query, animalsArray) {
+	let filteredResults = animalsArray;
+
+	let personalityTraitsArray = [];
+
+	if (query.personalityTraits) {
+		// making sure personality traits are saved as an array
+		if (typeof query.personalityTraits === "string") {
+			personalityTraitsArray = [query.personalityTraits];
+		} else {
+			personalityTraitsArray = query.personalityTraits;
+		}
+	}
+
+	// looping through personalityTraitsArray and filtering down the results
+	// each iteration removes all animals that don't have the current trait
+	// at the end, only animals that have all of them will be included
+	personalityTraitsArray.forEach((trait) => {
+		filteredResults = filteredResults.filter(
+			(animal) => animal.personalityTraits.indexOf(trait) !== -1
+		);
+	});
+
+	// using ifs and not else-ifs to account for there being multiple query parameters
+	if (query.diet) {
+		filteredResults = filteredResults.filter(
+			(animal) => animal.diet === query.diet
+		);
+	}
+
+	if (query.species) {
+		filteredResults = filteredResults.filter(
+			(animal) => animal.species === query.species
+		);
+	}
+	if (query.name) {
+		filteredResults = filteredResults.filter(
+			(animal) => animal.name === query.name
+		);
+	}
+
+	return filteredResults;
+}
+
+// important to not include the dot
+app.get("/api/animals", (req, res) => {
+	let results = animals;
+
+	if (req.query) {
+		results = filterByQuery(req.query, results);
+	}
+	// if there is a query parameter attached to the url, turn it into JSON and give it to filterByQuery() as a parameter
+
+	res.json(results);
+});
+// req and res stand for request and response
+// app.get takes two parameters:
+// 1. a route that tells where the data will come from
+// 2. a callback that will execute every time that route receives a GET request
+
+app.listen(3001, () => {
+	console.log("Express server on port 3001");
+});
